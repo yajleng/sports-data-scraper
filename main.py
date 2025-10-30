6fc6d8348ff265e62929728db53915b2ff6829c1
yajleng <lengfuyaj@gmail.com>
2025-10-30 19:03:49 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,6 @@ from flask import Flask, jsonify
 from modules.espn_nfl import get_nfl_data  # import the scraper function
 from modules.espn_nfl_teamstats import get_nfl_teamstats
 
-@app.route("/scrape/nfl/teamstats")
-def scrape_nfl_teamstats():
-    return jsonify(get_nfl_teamstats())
-
 app = Flask(__name__)
 
 @app.route("/")
@@ -26,6 +22,10 @@ def scrape_nfl():
     """Fetch live NFL data from ESPN."""
     data = get_nfl_data()
     return jsonify(data)
+    
+    @app.route("/scrape/nfl/teamstats")
+def scrape_nfl_teamstats():
+    return jsonify(get_nfl_teamstats())
     
 @app.route("/health")
 def health():
