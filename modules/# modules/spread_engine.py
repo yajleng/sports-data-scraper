05299e4fcd4661fa9bb4738780e347bad181950c
yajleng <lengfuyaj@gmail.com>
2025-11-01 12:05:05 -0400

    Delete modules/# modules directory

--- modules/# modules/spread_engine.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-def calculate_spread_edge(team_sp, opp_sp, line):
-    true_diff = team_sp - opp_sp
-    implied_win_prob = 1 / (1 + np.exp(-true_diff / 7))
-    edge = implied_win_prob - (abs(line) / 30)
-    return {"true_diff": true_diff, "win_prob": implied_win_prob, "edge": edge}
